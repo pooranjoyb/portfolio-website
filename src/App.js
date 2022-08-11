5ea940d4006e3084ecf04310139fdfe4f863833d
Pooranjoy Bhattacharya <pooranjoyb2016@gmail.com>
2022-08-11 19:01:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Content from "./components/Content";
+
+function App() {
+  return (
+
+    <>
+      <Navbar/>
+      <Content/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar navbar-light bg-light fixed-top">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Portfolio</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">MENU</h5>
+                <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Services</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Resume</a>
+                    
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+    </>
+  )
+}
