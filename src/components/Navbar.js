0113175eca223bb70cd388012c20cf7cb752d748
pooranjoyb <pooranjoyb2016@gmail.com>
2022-10-29 00:21:37 +0530

    Development server's img bug resolved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg">
   <div className="container-fluid">
-    <a className="navbar-brand" href="https://github.com/pooranjoyb/portfolio-website"><img src="img/web.png" width='30' alt=""/></a>
+    <a className="navbar-brand" href="https://github.com/pooranjoyb/portfolio-website"><img src="./portfolio-website/img/web.png" width='30' alt=""/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -9,28 +9,28 @@ export default function Skillset() {
                 </div>
                 <div className="row">
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/java-script.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/java-script.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/c-logo.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/c-logo.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/html-5-logo.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/html-5-logo.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/css-3.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/css-3.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/atom.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/atom.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/mongo.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/mongo.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/github.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/github.png" alt="JavaScript" />
                     </div>
                     <div className="icons col-md-2 col-4">
-                        <img src="./icons/bootstrap.png" alt="JavaScript" />
+                        <img src="./portfolio-website/icons/bootstrap.png" alt="JavaScript" />
                     </div>
                 </div>
             </div>
